75966630e91f07b6f3ec2dc85cd312115c2bb48f
KingOfBraves <chadifferus@gmail.com>
2018-04-16 09:18:17 -0300

    first commit

---
+++ GameLogic.java
@@ -0,0 +1,53 @@
+public class GameLogic extends GameSubject 
+{
+   private int gameStatus = GameStatus.gameContinue;
+   private GameReferee referee;
+   private GameBoard gameBoard;
+   private static GameLogic instance;
+   
+   private GameLogic() 
+   {
+   }
+
+   /*
+    * Returns a singleton of GameLogic
+    * @returns a GameLogic singleton
+    */
+   public GameLogic getInstance() {
+	   if (instance == null) {
+		   instance = new GameLogic();
+		   instance.setupGameBoard();
+	   }
+	   return instance;
+   }
+   
+   /*
+    * sets up the singleton with the required steps from the old constructor
+    */
+   private void setupGameBoard() {
+	   gameBoard = new GameBoard();
+       referee = new GameReferee(gameBoard);
+   }
+
+   public void makeMove(int row, int col, int player) 
+   {
+          int status = referee.checkLegalMove(row, col, player);
+          if (status != 0) {
+            gameStatus = GameStatus.illegalMove;
+          } else {
+            gameBoard.setPlayer(row, col, player);
+            gameStatus = referee.checkGameStatus();
+            notifyObservers();
+          }
+   }
+
+   public GameBoardView getGameBoard() 
+   {
+    return gameBoard;
+   }
+
+   public int getGameStatus() 
+   {
+    return gameStatus;
+   }
+}
---
+++ GameStatus.java
@@ -0,0 +1,8 @@
+public interface GameStatus 
+{
+   public static int gameContinue = 0;
+   public static int player1Win = 1;
+   public static int player2Win = 2;
+   public static int tie = 3;
+   public static int illegalMove = 4;
+}
